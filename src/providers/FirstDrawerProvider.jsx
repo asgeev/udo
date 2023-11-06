16f815148ec772201f934f195e508f2aad07a1fa
asgeev <adamszymanski27@hotmail.com>
2023-11-06 14:22:37 +0100

    Add modal prevent close without saving

--- src/providers/FirstDrawerProvider.jsx
+++ src/providers/FirstDrawerProvider.jsx
@@ -1,17 +1,40 @@
 import { useState, createContext } from 'react'
+import { Modal } from 'antd'
 
 export const FirstDrawerContext = createContext({})
 
 export const FirstDrawerProvider = ({ children }) => {
     const [isFirstDrawerVisible, setIsFirstDrawerVisible] = useState(null)
     const [isFormChanged, setIsFormChanged] = useState(false)
+    const { confirm } = Modal
+
+    window.onbeforeunload = () => {
+        if (isFormChanged) {
+            return 'Na pewno chcesz odświeżyć? Wprowadzone zmiany nie zostaną zapisane!'
+        } else {
+            return null
+        }
+    }
+
+    const showCloseConfirm = () => {
+        confirm({
+            title: 'Zaczekaj!',
+            content: 'Czy na pewno chcesz wyjść bez zapisywania!?',
+            destroyOnClose: true,
+            okText: 'Wyjdź',
+            onOk() {
+                setIsFirstDrawerVisible(false)
+                setIsFormChanged(false)
+            },
+        })
+    }
 
     const openDrawer = () => {
         setIsFirstDrawerVisible(true)
     }
 
     const closeDrawer = () => {
-        setIsFirstDrawerVisible(false)
+        isFormChanged ? showCloseConfirm() : setIsFirstDrawerVisible(false)
     }
 
     return (
