ebe5935155d0bd599c7f3f3e0c7c3faa0417041e
asgeev <asgeev@proton.me>
2025-06-26 13:09:14 +0000

    Refactor ShowTableStatusTags component to simplify status rendering and remove unused icons

--- src/components/atoms/ShowTableStatusTags/ShowTableStatusTags.jsx
+++ src/components/atoms/ShowTableStatusTags/ShowTableStatusTags.jsx
@@ -1,30 +1,12 @@
 import { Space, Tag, Tooltip } from 'antd'
-import {
-    CheckCircleOutlined,
-    SyncOutlined,
-    WarningOutlined,
-} from '@ant-design/icons'
 
 export const ShowTableStatusTags = ({ record }) => {
+    const { status } = record
     return (
         <Space wrap>
-            {!record.finished_status ? (
-                <Tag icon={<CheckCircleOutlined />} color="success">
-                    zakończono
-                </Tag>
-            ) : (
-                <Tag icon={<SyncOutlined />} color="processing">
-                    w trakcie
-                </Tag>
-            )}
-            {(!record.koszulka_id || !record.nr_sprawy) && (
-                <Tooltip
-                    title="Brak utworzonej koszulki lub sprawy"
-                    color="red"
-                >
-                    <Tag icon={<WarningOutlined />} color="error">
-                        Błąd
-                    </Tag>
+            {status && (
+                <Tooltip title={status?.description}>
+                    <Tag title={status.type}>{status.type}</Tag>
                 </Tooltip>
             )}
         </Space>
